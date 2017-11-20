58415041235f611dd51ddeff53ab1d367c9b1e29
dimiturtrz <dimitar.trz@gmail.com>
2017-11-20 22:59:03 +0200

    first part of homework 2 - not full collection, not full element, not full reading

---
+++ homework/hw2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include "MyStrings.h"
+
+using namespace std;
+
+int main (int argc, const char * argv[]) {
+	const char* fileName = argv[1];
+	const char* sortArg = argv[2];
+	const char* sortDir = argv[3];
+
+	ifstream file;
+	file.open(fileName);
+	
+
+	return 0;
+}
