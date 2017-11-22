94b99d0f002b982aec25d135ac8658d60527d051
dimiturtrz <dimitar.trz@gmail.com>
2017-11-22 08:23:21 +0200

    making the file reading. Just have to check something

--- homework/hw2/main.cpp
+++ homework/hw2/main.cpp
@@ -5,24 +5,48 @@
 
 using namespace std;
 
+void readCollectionFromFile(StudentCollection& collection, const char* fileName);
+
 int main (int argc, const char * argv[]) {
 	const char* fileName = argv[1];
 	const char* sortArg = argv[2];
 	const char* sortDir = argv[3];
 
-	ifstream file;
-	file.open(fileName);
-	
 	StudentCollection sc;
-	Student st1("pesho", 12, "js", 3, "2012-12-05");
-	sc.add(st1);
-	Student st2("tesho", 12, "js", 3, "2012-12-05");
-	sc.add(st2);
-	Student st3("gesho", 12, "js", 3, "2012-12-05");
-	sc.add(st3);
+
+	readCollectionFromFile(sc, fileName);
+
 	sc.print();
-	
-	file.close();
 
 	return 0;
+}
+
+void readCollectionFromFile(StudentCollection& collection, const char* fileName) {
+	ifstream file;
+	file.open(fileName);
+
+	char nameBuffer[1024];
+	int age;
+	char subjectBuffer[1024];
+	int grade;
+	char dateBuffer[1024];
+
+	while(file) {
+		if(file.peek() == -1) { // TODO: check!!!
+			break;
+		}
+
+		file.getline(nameBuffer, 1023, ',');
+		file>> age;
+		file.ignore();
+		file.getline(subjectBuffer, 1023, ',');
+		file>> grade;
+		file.ignore();
+		file.getline(dateBuffer, 1023, '\n');
+	
+		Student readStudent(nameBuffer, age, subjectBuffer, grade, dateBuffer);
+		collection.add(readStudent);
+	}
+	
+	file.close();
 }
